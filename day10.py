117391689230d1f4737cfa507a3699a453489c79
Fredrik Mile <fredrik.mile@gmail.com>
2021-12-10 08:20:25 +0100

    Formatted and cleaned day10.py

--- day10.py
+++ day10.py
@@ -1,20 +1,21 @@
 with open("input.txt") as f:
     input = f.read().splitlines()
 
+
 def get_closing(c):
-    if c == '<':
-        return '>'
-    if c == '(':
-        return ')'
-    if c == '[':
-        return ']'
-    if c == '{':
-        return '}'
+    if c == "<":
+        return ">"
+    if c == "(":
+        return ")"
+    if c == "[":
+        return "]"
+    if c == "{":
+        return "}"
 
 s = 0
-openings = ['[', '<', '{', '(']
-closing = [']', '>', '}', ')']
-points1 = {')':3, ']':57, '}':1197, '>':25137}
+openings = ["[", "<", "{", "("]
+closing = ["]", ">", "}", ")"]
+points = {")": 3, "]": 57, "}": 1197, ">": 25137}
 remains = []
 for line in input:
     open = []
@@ -26,22 +27,21 @@ for line in input:
             if open[-1] == openings[i]:
                 open = open[:-1]
             else:
-                s += points1[c]
+                s += points[c]
                 break
     else:
         if len(open) > 0:
             remains.append([get_closing(o) for o in reversed(open)])
-print(s)
+print("Part A: " + str(s))
 
 scores = []
-points2 = {')':1, ']':2, '}':3, '>':4}
+points = {")": 1, "]": 2, "}": 3, ">": 4}
 for remain in remains:
     score = 0
     for c in remain:
-        score = score * 5
-        score += points2[c]
+        score *= 5
+        score += points[c]
     scores.append(score)
 
-middle_index = int((len(scores) - 1) / 2)
 scores.sort()
-print(scores[middle_index])
+print("Part B: " + str(scores[int((len(scores) - 1) / 2)]))
