ea57ca2ba9787541687e3c17d45d71bddbc6b48b
Fredrik Mile <fredrik.mile@gmail.com>
2021-12-12 12:57:31 +0100

    Formatted day12.py

--- day12.py
+++ day12.py
@@ -6,38 +6,44 @@ with open("input.txt") as f:
 graph = defaultdict(list)
 
 for line in input:
-    from_cave = line.split('-')[0]
-    to_cave = line.split('-')[1]
+    from_cave = line.split("-")[0]
+    to_cave = line.split("-")[1]
     graph[from_cave].append(to_cave)
     graph[to_cave].append(from_cave)
 
 paths = []
+
+
 def traverse_graph_part_one(graph, current_node, path):
     path.append(current_node)
     for v in graph[current_node]:
         if v not in path or v.isupper():
             traverse_graph_part_one(graph, v, path.copy())
-    paths.append(path)
-    pass
+    if path[-1] == "end":
+        paths.append(path)
+
 
 traverse_graph_part_one(graph, "start", [])
-paths = [path for path in paths if path[-1] == "end"]
 print("Part A: " + str(len(paths)))
 
-def small_cave_twice(path):
+
+def path_only_single_small_cave_twice(path):
     lowercases = [n for n in path if n.islower()]
     return len(lowercases) == len(set(lowercases))
 
+
 paths = []
+
+
 def traverse_graph_part_two(graph, current_node, path):
     path.append(current_node)
     for v in graph[current_node]:
-        if v not in path or v.isupper() or small_cave_twice(path):
+        if v not in path or v.isupper() or path_only_single_small_cave_twice(path):
             traverse_graph_part_two(graph, v, path.copy())
-    paths.append(path)
-    pass
+    if path[-1] == "end" and path.count("end") == 1 and path.count("start") == 1:
+        paths.append(path)
+
 
 traverse_graph_part_two(graph, "start", [])
 
-paths = [path for path in paths if path[-1] == "end" and path.count('end') == 1 and path.count('start') == 1]
 print("Part B: " + str(len(paths)))
