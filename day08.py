59bdb36bb4a64fb6ebc8e68ed144ccf9925e6dde
Fredrik Mile <fredrik.mile@gmail.com>
2021-12-12 10:47:11 +0100

    Add day08.py, only part A

---
+++ day08.py
@@ -0,0 +1,32 @@
+
+"""
+  0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+"""
+
+
+with open("input.txt") as f:
+    input = f.read().splitlines()
+
+c = 0
+for line in input:
+    output = line.split(' | ')[1]
+    for signal in output.split():
+         if len(signal) in [2, 3, 4, 7]:
+             c += 1
+print("Part A: " + str(c))
