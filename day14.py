26c55dd23921b07fd120ddab58ae0d073f31bf8a
Fredrik Mile <fredrik.mile@gmail.com>
2021-12-15 09:05:45 +0100

    Add day14.py, only part A

---
+++ day14.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+with open("input.txt") as f:
+    input = f.read().splitlines()
+
+
+STEPS = 40
+
+insertions = dict()
+template = ""
+
+for line in input:
+    if line and " -> " in line:
+        left = line.split(" -> ")[0]
+        right = line.split(" -> ")[1]
+        insertions[left] = right
+    elif line:
+        template = line
+
+
+for _ in range(STEPS):
+    new_template = template[0]
+    for i in range(len(template) - 1):
+        pair = template[i : i + 2]
+        new_template += insertions.get(pair) + pair[1]
+    template = new_template
+
+sorted_occurences = Counter(template).most_common()
+
+print(f"Part A: {sorted_occurences[0][1] - sorted_occurences[-1][1]}")
