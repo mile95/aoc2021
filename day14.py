d87b902edcf38ee2ee222fa7846c2396bc1ced60
Fredrik Mile <fredrik.mile@gmail.com>
2021-12-17 09:10:24 +0100

    Update day14.py with solution for part 2

--- day14.py
+++ day14.py
@@ -1,11 +1,9 @@
 from collections import Counter
+from collections import defaultdict
 
 with open("input.txt") as f:
     input = f.read().splitlines()
-
-
-STEPS = 10
-
+ 
 insertions = dict()
 template = ""
 
@@ -17,14 +15,39 @@ for line in input:
     elif line:
         template = line
 
-
-for _ in range(STEPS):
-    new_template = template[0]
+def compute(steps):
+    occurences = defaultdict(int)
     for i in range(len(template) - 1):
-        pair = template[i : i + 2]
-        new_template += insertions.get(pair) + pair[1]
-    template = new_template
+        occurences[template[i:i+2]] += 1
 
-sorted_occurences = Counter(template).most_common()
+    for _ in range(steps):
+        copy = occurences.copy()
+        for k,v in occurences.items():
+            if v > 0:
+                new_char = insertions.get(k)
+                new_pair_1 = k[0] + new_char
+                new_pair_2 = new_char + k[1]
+                copy[new_pair_1] += v
+                copy[new_pair_2] += v
+                copy[k] -= v
+                if copy[k] < 0:
+                    raise Exception
+        occurences = copy
 
-print(f"Part A: {sorted_occurences[0][1] - sorted_occurences[-1][1]}")
+    chars = ""
+    for c in occurences.keys():
+        chars += c
+
+    chars = set(chars)
+    counts = defaultdict(int)
+    for c in chars:
+        for k in occurences.keys():
+            if c == k[0]:
+                counts[c] += occurences[k]
+
+    counts[template[-1]] += 1
+    return max(counts.values()) - min(counts.values())
+
+
+print(f"Part A: {compute(10)}")
+print(f"Part B: {compute(40)}")
